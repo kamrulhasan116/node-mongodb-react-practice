d01a2c72def76598780ff13a2757e3ff51460668
kamrulhasan116 <hasan.kamrul116@gmail.com>
2022-12-13 13:40:40 -0800

    re added file

--- config/db.js
+++ config/db.js
@@ -3,10 +3,11 @@ const config = require("./config");
 mongoose.set('strictQuery', true);
 
 const dbUrl = config.db.url;
-mongoose.connect(dbUrl, {useNewUrlParser: true,
+const connectDb = mongoose.connect(dbUrl, {useNewUrlParser: true,
     useUnifiedTopology: true}).then(()=>{
     console.log("Db is connected")
 }).catch((error)=>{
     console.log(error);
     process.exit(1);
 })
+module.exports = connectDb;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 const config = require("./config/config");
-const { connectDb } = require("./config/db");
+const connectDb  = require("./config/db");
 const port = config.app.port;
 
-connectDb().then(() =>{
+connectDb.then(() =>{
     app.listen(port, ()=>{
         console.log(`app is running :${port}`);
     })
