2bb347fff49257ad27c0ae6c3f2d812026d03c57
kamrulhasan116 <hasan.kamrul116@gmail.com>
2022-12-13 13:22:49 -0800

    db connection

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const app = require("./app");
 const config = require("./config/config");
+const { connectDb } = require("./config/db");
 const port = config.app.port;
 
-app.listen(port, ()=>{
-    console.log(`app is running :${port}`);
+connectDb().then(() =>{
+    app.listen(port, ()=>{
+        console.log(`app is running :${port}`);
+    })
 })
